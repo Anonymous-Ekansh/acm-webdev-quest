3740dda708c5aaebe29cda09a5594555a4f0fdd0
Ekansh <ekanshjain329@gmail.com>
2026-06-02 18:12:50 +0530

    Day 2 Input Component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,5 +1,46 @@
 import './Input.css';
 
-export const Input = () => {
-  return <div></div>;
+export const Input = (props) => {
+
+  let classes = 'input';
+
+  if (props.error) {
+    classes = classes + ' input-error';
+  }
+
+  if (props.disabled) {
+    classes = classes + ' input-disabled';
+  }
+
+  return (
+    <div className="input-wrapper">
+
+      {props.leftIcon ? (
+        <span className="input-icon-left">
+          {props.leftIcon}
+        </span>
+      ) : null}
+
+      <input
+        className={classes}
+        placeholder={props.placeholder}
+        defaultValue={props.defaultValue}
+        disabled={props.disabled}
+        onChange={props.disabled ? undefined : props.onChange}
+      />
+
+      {props.rightIcon ? (
+        <span className="input-icon-right">
+          {props.rightIcon}
+        </span>
+      ) : null}
+
+      {props.error ? (
+        <span role="alert">
+          {props.error}
+        </span>
+      ) : null}
+
+    </div>
+  );
 };
